b08f72ec6af5d34a847a5b2f3b36b59bcaa7f12c
gjdickens <geeregg@gmail.com>
2016-10-23 14:53:54 +0100

    changed port

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(port);
